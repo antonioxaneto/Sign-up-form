d0d1e3734dc33a063be19937c8b21317611a8ed4
Antonio Neto <antonio___neto@outlook.com>
2024-09-15 10:02:17 -0300

    ajustes final em todo código para finalizar projeto

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const email = document.getElementById('email');
 const form = document.getElementById('form');
 const errorMessage = document.querySelector('.error-message');
+const modal = document.getElementById('success-modal');
+const modalOverlay = document.getElementById('modal-overlay');
+const closeModalButton = document.getElementById('close-modal');
+
+const modalMessage = modal.querySelector('p');
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -14,6 +19,10 @@ email.addEventListener('input', () => {
     }
   });
 
+closeModalButton.addEventListener('click', () => {
+    closeModal();
+  });
+
 function checkInput(){
     const emailValue = email.value.trim()
 
@@ -21,6 +30,7 @@ function checkInput(){
         errorInput(email, 'Valid email required')
     }else{
         enviarEmail();
+        showModal(email.value);
     }
 }
 
@@ -32,10 +42,27 @@ function errorInput(input, message) {
     input.classList.add('error');
     errorMessage.style.display = 'block';
     errorMessage.innerText = message;
-  }
+}
 
-  function removeError(input) {
+function removeError(input) {
     input.classList.remove('error');
     errorMessage.style.display = 'none';
-  }
+}
+  
+function showModal() {
+    modal.style.display = 'block';
+    modalOverlay.style.display = 'block';
+}
+
+function showModal(emailValue) {
+    modalMessage.innerHTML = `A confirmation email has been sent to <strong>${emailValue}</strong>. Please open it and click the button inside to confirm your subscription.`;
+    
+    modal.style.display = 'block';
+    modalOverlay.style.display = 'block';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+    modalOverlay.style.display = 'none';
+}
 
